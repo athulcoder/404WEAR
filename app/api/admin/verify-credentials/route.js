cbe23fe9aa6b5c6d68e8ad0540962d3b62b14400
athulcoder <athul.sin.cos.tan@gmail.com>
2026-04-30 17:17:08 +0530

    otp sending done

--- app/api/admin/verify-credentials/route.js
+++ app/api/admin/verify-credentials/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
-import { maskEmail } from "@/lib/email";
+import { maskEmail, sendOTPEmail } from "@/lib/email";
 import { generateOTP, storeOTP } from "@/lib/otp";
 export async function POST (req){   
     const {username , password} =  await req.json()
@@ -32,6 +32,10 @@ export async function POST (req){
     const maskedEmail = maskEmail(admin.email);
     const otpCode = generateOTP();
     storeOTP(admin.id,otpCode);
+    const res = await sendOTPEmail(admin.email,otpCode)
+
+    if (!res.success)
+        return NextResponse.json({message:"Invalid username or password | couldn't send email"}, {status:400})
     
     return NextResponse.json({message:`Credentials are valid. OTP sent to ${maskedEmail}`},{
         status:200
